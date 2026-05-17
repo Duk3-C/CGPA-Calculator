1fb71ffb165015669eb56f09f6765aba04e94097
Duk3-C <orlandojmarotogarcia@proton.me>
2026-05-16 21:29:45 -0400

    Added Semester object for storing class data. Compiler is not working somehow

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -12,13 +13,24 @@ struct Student
     Student(string s_name, string c_name) : student_name(move(s_name)), class_name(move(c_name)) {};
 };
 
+struct Semester
+{
+    double class_credits;
+    double class_grades;
+
+    Semester(double c_credits, double c_grades) : class_credits(c_credits), class_grades(c_grades) {};
+};
+
 int main()
 {
     system("clear");
 
     vector<Student> studentInfo;
+    vector<Semester> class_data;
     string student_name;
     string class_name;
+    double class_credits;
+    double class_grades;
 
     cout << "==============================\n" <<
             "==     CGPA Calculator      ==\n" <<
@@ -28,15 +40,33 @@ int main()
     cout<<"\n";
     cout << "Enter the name of the class your student is currently enrolled in: ";
     getline(cin, class_name);
-    
+    cout << "\n";
+
     studentInfo.emplace_back(student_name, class_name);
 
+    cout<<"Enter how many credits are given by your class: ";
+    cin >> class_credits;
+    cout << "\n\n";
+    class_data.emplace_back(class_credits);
+
+    cout << "Enter your student's grades in each semester: ";
+    for(int i=0; i < 4; i++)
+    {
+        cin >> class_grades;
+        class_data.emplace_back(class_grades);
+    }
+
+    /*DEBUG for class_data vector*/
+    for(const auto& classs : class_data)
+    {
+        cout << classs.class_credits << " " << classs.class_grades << "\n";
+    }
     /*
-    DEBUG 
+    DEBUG for studentInfo vector
     
     for(const auto& student : studentInfo)
     {
-        cout << student.student_name << student.class_name << "\n";
+        cout << student.student_name << " " << student.class_name << "\n";
     }
     */
 
